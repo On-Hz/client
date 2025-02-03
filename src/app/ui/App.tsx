ed4847cea08434842fcf950884b2127c5bbf0438
sandume8 <sandume8@gmail.com>
2025-02-03 17:08:02 +0900

    chore: 라이브러리 추가 및 기본 폴더구조 생성

---
+++ src/app/ui/App.tsx
@@ -0,0 +1,8 @@
+import { FunctionComponent } from 'react'
+
+export const App: FunctionComponent = () => (
+      <div>
+          <h2 className="text-xl font-bold text-blue-500">Hello, React!</h2>
+          <p className="text-lg font-medium">Hello, Typescript!</p>
+      </div>
+)
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { App } from './app';
+import './main.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const reactRoot = createRoot(
+  document.getElementById('root')!,
+)
+
+reactRoot.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>
 )
