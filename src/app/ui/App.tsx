a50c2917d62eebb9e1acd5191e42a65df35208a0
sooyangg <o0ooo10044@gmail.com>
2025-02-14 21:03:37 +0900

    Mui 적용, UI가이드에 따른 tailwind.config.js colors 추가

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -3,6 +3,6 @@ import { FunctionComponent } from 'react'
 export const App: FunctionComponent = () => (
       <div>
           <h2 className="text-xl font-bold text-blue-500">Hello, React!</h2>
-          <p className="text-lg font-medium">Hello, Typescript!</p>
+          <p className="text-lg font-medium text-point">Hello, Typescript!</p>
       </div>
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: { // UI가이드에 따른 대표 컬러 지정
+        transparent: 'transparent',
+        point: '#395EA1',
+        black: '#222',
+        gray: '#5E5B65',
+      },
+    },
   },
   plugins: [],
 }
