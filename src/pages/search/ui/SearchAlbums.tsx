a2837f72a2d4f40e497b05d24f626d10f43edbc2
sandume8 <sandume8@gmail.com>
2025-02-17 22:28:32 +0900

    Style: 오타 수정

--- src/pages/search/ui/SearchAlbums.tsx
+++ src/pages/search/ui/SearchAlbums.tsx
@@ -20,8 +20,8 @@ export const SearchAlbums: React.FC = () => {
   return (
     <section className="px-4 py-10 mx-auto max-w-7xl">
       <div className="flex items-center justify-between mb-10">
-        <h2 className="text-2xl font-bold">아티스트</h2>
-      </div>{" "}
+        <h2 className="text-2xl font-bold">앨범</h2>
+      </div>
       <div className="grid grid-cols-6 gap-5">
         {mockAlbums.map((album) => (
           <div
