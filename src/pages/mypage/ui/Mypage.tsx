2fc24ac4fa6317236c10328740a8ded33e9318a5
sooyangg <o0ooo10044@gmail.com>
2025-02-27 22:53:55 +0900

    Rename: 마이페이지 import './style.css'

--- src/pages/mypage/ui/Mypage.tsx
+++ src/pages/mypage/ui/Mypage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { MypageInfo } from "../info/ui/MypageInfo";
 import { Outlet } from "react-router-dom";
+import './style.css'
 
 export const MyPage: React.FC = () => {
     return (
