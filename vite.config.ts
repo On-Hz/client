12fd29c017af9272780b2c68393e34e87016ab97
sooyangg <o0ooo10044@gmail.com>
2025-03-06 22:57:37 +0900

    vite.config.ts build 추가

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,16 @@ import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()]
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    // 엔트리 파일과 청크 파일 이름을 고정
+    rollupOptions: {
+      output: {
+        // 엔트리 파일의 이름을 'index.js'로 고정
+        entryFileNames: 'index.js',
+        // 청크 파일의 이름을 고정 (필요시 수정)
+        chunkFileNames: 'assets/[name].js',
+      }
+    }
+  }
 })
