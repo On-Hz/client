9ad694f6853346bde98091abaec39eb3929d1c5c
sandume8 <sandume8@gmail.com>
2025-03-10 18:41:21 +0900

    chore: ecosystem.config 수정

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,11 +2,11 @@ module.exports = {
   apps: [{
     name: "on-hz",
     script: "serve",
-    args: "dist 6000 --single", // 첫번째 인자는 서빙할 폴더, 두번째는 포트, --single은 SPA 모드
-    exec_mode: "cluster",
-    instances: "max",
     env: {
-      NODE_ENV: "production"
+      PM2_SERVE_PATH: "/app/dist",
+      PM2_SERVE_PORT: 8000,
+      PM2_SERVE_SPA: "true",
+      PM2_SERVE_HOST: "0.0.0.0"  // 모든 인터페이스에서 바인딩
     }
   }]
 }
