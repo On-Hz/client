a7fbcb828cba725ae511f195e080333744d062c3
sandume8 <sandume8@gmail.com>
2025-03-31 22:24:27 +0900

    fix: 장르 별 앨범 리스트 api parameter 삭제

--- src/features/genre/list/api/getAlbumsByGenreApi.ts
+++ src/features/genre/list/api/getAlbumsByGenreApi.ts
@@ -4,12 +4,7 @@ import { Album } from "@/shared/model";
 
 const getAlbumsByGenre = async (genreCode: string) => {
   const url = `/api/v1/albums/genre/${genreCode}/featured`;
-  const response = await axiosInstance.get<Album[]>(url, {
-    params: {
-      offset: 0,
-      limit: 25
-    },
-  });
+  const response = await axiosInstance.get<Album[]>(url);
   return response.data;
 };
 
