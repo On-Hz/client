f8a246ed8107558a58886ba49ee4d03c54fc1998
sooyangg <o0ooo10044@gmail.com>
2025-04-22 18:45:08 +0900

    Design: 사업자번호, 회사안내 제거

--- src/widgets/footer/ui/Footer.tsx
+++ src/widgets/footer/ui/Footer.tsx
@@ -17,14 +17,8 @@ export const Footer = () => {
             to="/privacy"
             className="text-gray5 text-[12px] mr-[4px] transform hover:text-gray2 transition-colors"
           >
-            개인정보 처리방침 |
+            개인정보 처리방침 
           </Link>
-          <a
-            href=""
-            className="text-gray5 text-[12px] mr-[4px] transform hover:text-gray2 transition-colors"
-          >
-            회사 안내
-          </a>
         </div>
         <div className="flex items-end">
           <p className="text-gray5 text-[12px] mr-[4px]">고객센터 |</p>
@@ -39,7 +33,6 @@ export const Footer = () => {
           <p className="text-gray5 text-[12px] mr-[4px]">On-Hz |</p>
           <p className="text-gray5 text-[12px] mr-[4px]">대표 웹얼라</p>
         </address>
-        <p className="text-gray5 text-[12px]">사업자등록번호 : 310-23-02087</p>
         <p className="pt-[20px] text-gray5 text-[12px]">
           © 2025 by On-Hz, Inc. All rights reserved.
         </p>
