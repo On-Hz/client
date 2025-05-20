5dd70db9857fd7be6f570dba4141314300ebf155
sooyangg <o0ooo10044@gmail.com>
2025-05-20 23:11:22 +0900

    Fix: 리뷰정렬 - 에러 이슈 임시제거

--- src/shared/constants/reviewSortOptions.ts
+++ src/shared/constants/reviewSortOptions.ts
@@ -2,9 +2,9 @@ import { ORDER_BY } from "./orderByTypes";
 
 export const REVIEW_SORT_OPTIONS = [
   { label: "최신순", value: ORDER_BY.CREATED_AT_DESC },
-  { label: "오래된순", value: ORDER_BY.CREATED_AT_ASC },
-  { label: "별점 많은순", value: ORDER_BY.RATING_AT_DESC },
-  { label: "별점 적은순", value: ORDER_BY.RATING_AT_ASC },
-  { label: "좋아요 많은순", value: ORDER_BY.LIKE_COUNT_AT_DESC },
-  { label: "좋아요 적은순", value: ORDER_BY.LIKE_COUNT_AT_ASC },
+  // { label: "오래된순", value: ORDER_BY.CREATED_AT_ASC },
+  { label: "별점순", value: ORDER_BY.RATING_AT_DESC },
+  // { label: "별점 적은순", value: ORDER_BY.RATING_AT_ASC },
+  { label: "좋아요순", value: ORDER_BY.LIKE_COUNT_AT_DESC },
+  // { label: "좋아요 적은순", value: ORDER_BY.LIKE_COUNT_AT_ASC },
 ];
